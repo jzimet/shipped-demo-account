17985f81f84f68e0b78f1a3885a0a77f1dc9285d
Vijay <vjpscjp@gmail.com>
2016-04-04 08:42:08 -0500

    updated  latest api
    Updated code api from nick public repo's

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# shipped-demo-account
-Account microservice for shopping site demo.
+# Account API
+
+## Overview
+This is a small Microservice example of simple Shopping Account API endpoint to be used with [Shipped](http://shipped-cisco.com).
+
+## Getting Started
+1. Simple clone the repo or download the repo and cd into the repo and do `go run account.go` to start the server on localhost.
+2. Then run this simple curl command to make sure the service is up and running `curl -i http://localhost:8000` or view it on your web browser
+
+```
+Expected Result
+
+HTTP/1.1 200 OK
+Date: Tue, 19 Jan 2016 15:52:02 GMT
+Content-Length: 160
+Content-Type: text/html; charset=utf-8
+
+<html>
+  <head>
+    <title>An example layout</title>
+  </head>
+  <body>
+
+<p>Account is up and running.</p>
+<p>Try This command</p>
+<p></p>
+
+  </body>
+</html>
+
+```
+
+## Endpoints
+
+- **POST /v1/account/**
+```
+curl -i -H "Content-Type: application/json" -X POST -d '{"username":"nick","password":"123"}' http://localhost:8000/v1/account/
+Expected Result:
+{
+    "success":true,
+    "token": 12325343,
+    "message":"Successfully logged in"
+}
+or curl -H "Content-Type: application/json" -X POST -d @loginExamplePost.json http://localhost:8000/v1/account/
+```
+- **GET /v1/account/**
+```
+curl -i -H "Content-Type: application/json" -X GET http://localhost:8000/v1/account/
+Expected Result:
+{
+    "success":true,
+    "message":"Successfully logged out in"
+}
+```
+
+## Requirements
+* [Go](https://github.com/golang/example)
+
+## Credits
+- [Nick Hayward](https://github.com/nehayward)
---
+++ account.go
@@ -0,0 +1,229 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"text/template"
+)
+
+// Login request JSON
+type Login struct {
+	UserName string `json:"username"`
+	Password string `json:"password"`
+}
+
+// Users a struct of user
+type Users struct {
+	Users []User `json:"users"`
+}
+
+type User struct {
+	UserName string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Response struct {
+	Status  string `json:"status"`
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+const error = "ERROR"
+const success = "SUCCESS"
+
+type AuthResponse struct {
+	AccessToken string `json:"accessToken"`
+	UserName    string `json:"username"`
+}
+
+func main() {
+	http.HandleFunc("/", HandleIndex)
+	http.HandleFunc("/v1/account/", account)
+
+	listenPort := getenv("SHIPPED_CATALOG_LISTEN_PORT", "8888")
+
+	log.Println("Listening on Port: " + listenPort)
+	http.ListenAndServe(fmt.Sprintf(":%s", listenPort), nil)
+}
+
+// Get environment variable.  Return default if not set.
+func getenv(name string, dflt string) (val string) {
+	val = os.Getenv(name)
+	if val == "" {
+		val = dflt
+	}
+	return val
+}
+
+func account(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Set("Content-Type", "application/json")
+	rw.Header().Set("Access-Control-Allow-Origin", "*")
+
+	switch req.Method {
+	case "GET":
+		// Logout
+		file, e := ioutil.ReadFile("./session.json")
+		if e != nil {
+			log.Printf("File error: %v\n", e)
+			os.Exit(1)
+		}
+
+		// Load JSON File
+		var user User
+		json.Unmarshal(file, &user)
+
+		if user.UserName != "" {
+			// Response
+			rw.WriteHeader(http.StatusNotFound)
+			success := response(success, http.StatusNotFound, "Logged out user "+user.UserName)
+			rw.Write(success)
+			log.Println("Succesfully logged out user " + user.UserName)
+
+			// Clean up
+			os.Remove("session.json")
+			os.Create("session.json")
+		} else {
+			// No session
+			rw.WriteHeader(http.StatusNoContent)
+			err := response(error, http.StatusNoContent, "No user logged in")
+			rw.Write(err)
+			log.Println("No user logged in")
+		}
+	case "POST":
+		// Login
+		body, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		// log.Println(string(body))
+
+		var t Login
+		err = json.Unmarshal(body, &t)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println(t.UserName)
+
+		// Store Login
+		loginIsSuccessful := verify(t.UserName, t.Password)
+
+		if loginIsSuccessful {
+			// Return Token Instead ** Always return JSON, Also have response Code
+			rw.WriteHeader(http.StatusAccepted)
+			success := AuthResponse{randToken(), t.UserName}
+			response, _ := json.MarshalIndent(success, "", "    ")
+			rw.Write(response)
+			log.Println("Succesfully logged in user " + t.UserName)
+		} else {
+			rw.WriteHeader(http.StatusUnauthorized)
+			error := response(error, http.StatusUnauthorized, "Login "+t.UserName+" failed.")
+			rw.Write(error)
+			log.Println("Login failed" + t.UserName)
+		}
+
+	case "PUT":
+		// Update an existing record.
+		rw.WriteHeader(http.StatusMethodNotAllowed)
+		err := response(error, http.StatusMethodNotAllowed, req.Method+" not allowed")
+		rw.Write(err)
+	case "DELETE":
+		// Update an existing record.
+		rw.WriteHeader(http.StatusMethodNotAllowed)
+		err := response(error, http.StatusMethodNotAllowed, req.Method+" not allowed")
+		rw.Write(err)
+	default:
+		// Give an error message.
+		rw.WriteHeader(http.StatusBadRequest)
+		err := response(error, http.StatusBadRequest, "Bad request")
+		rw.Write(err)
+	}
+}
+
+func verify(username string, password string) bool {
+	file, e := ioutil.ReadFile("users.json")
+	if e != nil {
+		fmt.Printf("File error: %v\n", e)
+		os.Exit(1)
+	}
+
+	var jsontype Users
+	err := json.Unmarshal(file, &jsontype)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for i := 0; i < len(jsontype.Users); i++ {
+		if jsontype.Users[i].UserName == username && jsontype.Users[i].Password == password {
+			// Save Login
+			session(jsontype.Users[i], true)
+			return true
+		}
+	}
+	return false
+}
+
+func response(status string, code int, message string) []byte {
+	resp := Response{status, code, message}
+	log.Println(resp.Message)
+	response, _ := json.MarshalIndent(resp, "", "    ")
+
+	return response
+}
+
+func session(user User, login bool) {
+	// Clear File
+	os.Remove("session.json")
+	os.Create("session.json")
+
+	file, e := os.OpenFile("session.json", os.O_RDWR|os.O_APPEND, 0660)
+	defer file.Close()
+	if e != nil {
+		log.Printf("File error: %v\n", e)
+		return
+	}
+
+	if login {
+		b, err := json.Marshal(user)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		d1 := []byte(b)
+		file.Write(d1)
+	}
+}
+
+func randToken() string {
+	b := make([]byte, 8)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)
+}
+
+// HandleIndex this is the index endpoint will return 200
+func HandleIndex(rw http.ResponseWriter, req *http.Request) {
+	lp := path.Join("templates", "layout.html")
+	fp := path.Join("templates", "index.html")
+
+	// Note that the layout file must be the first parameter in ParseFiles
+	tmpl, err := template.ParseFiles(lp, fp)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(rw, nil); err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+	}
+
+	// Give a success message.
+	rw.WriteHeader(http.StatusOK)
+	success := response(success, http.StatusOK, "Ready for request.")
+	rw.Write(success)
+}
